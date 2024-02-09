b311ec45c11a01bc3ec9c6689f1ea9b3986b2064
Xiyuan Tu <xiyuan.tyler@gmail.com>
2024-02-08 19:37:20 -0800

    first commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import logo from './logo.svg';
+import './App.css';
+import MyCalendar from './component/basicCalendar';
+
+function App() {
+  return (
+    <MyCalendar></MyCalendar>
+  );
+}
+
+export default App;
