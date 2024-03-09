24d75cbc3056177664c3ce373a15c0a4255f5e5a
Xiyuan Tu <xiyuan.tyler@gmail.com>
2024-03-09 00:20:19 -0800

    add navbar layout

--- src/component/NavBar/NavBar.js
+++ src/component/NavBar/NavBar.js
@@ -1,9 +1,26 @@
-import { Box } from '@mui/material'
+import { Box, Divider } from '@mui/material'
 import React from 'react'
 import { useCreateSlotsMutation, useGetSlotsQuery } from '../../api/apiSlice'
 import { SlotAction } from './SlotAction'
+import Logo from './Logo'
+import { Profile } from './Profile'
+import LogOut from './LogOut'
+import CoachSelection from './CoachSelection'
 
 export const NavBar = () => {
-    return <Box><SlotAction /></Box>
+    return <Box sx={{ height: '100%' }}>
+        <Box sx={{ height: '100%', display: 'flex', flexDirection: 'row', alignItems: 'center', paddingX: 10, justifyContent: 'space-between' }}>
+            <Logo />
+            <CoachSelection />
+            <SlotAction />
+            <Box sx={{ display: 'flex', flexDirection: 'row', alignItems: 'center', }}>
+                <Profile />
+                <LogOut />
+            </Box>
+
+        </Box>
+
+        <Divider />
+    </Box>
 
 }
